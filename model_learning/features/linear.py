f5c46460a34e99ed8d0a9a328e9f4982fc232812
Pedro Sequeira <pedrodbs@gmail.com>
2022-05-26 20:55:27 -0700

    modified reward features and linear vectors and object-world specific features

--- model_learning/features/linear.py
+++ model_learning/features/linear.py
@@ -1,9 +1,12 @@
 import operator
 import numpy as np
 from abc import ABC, abstractmethod
+from typing import Optional, List
 from psychsim.agent import Agent
-from psychsim.pwl import VectorDistributionSet, rewardKey, setToConstantMatrix, makeTree, setToFeatureMatrix, \
-    KeyedTree, KeyedPlane, KeyedVector, actionKey
+from psychsim.pwl import rewardKey, setToConstantMatrix, makeTree, setToFeatureMatrix, KeyedTree, KeyedPlane, \
+    KeyedVector, actionKey
+from psychsim.world import World
+from model_learning import PsychSimType, State
 
 __author__ = 'Pedro Sequeira'
 __email__ = 'pedrodbs@gmail.com'
@@ -11,59 +14,12 @@ __email__ = 'pedrodbs@gmail.com'
 COMPARISON_OPS = {'==': operator.eq, '>': operator.gt, '<': operator.lt}
 
 
-class LinearRewardVector(object):
-    """
-    Represents a linear reward vector, i.e., a reward function formed by a linear combination of a set of reward
-    features.
-    """
-
-    def __init__(self, rwd_features):
-        """
-        Creates a new reward vector with the given features.
-        :param list[LinearRewardFeature] rwd_features: the list of reward features.
-        """
-        self.rwd_features = rwd_features
-        self.names = [feat.name for feat in self.rwd_features]
-
-    def __len__(self):
-        return len(self.rwd_features)
-
-    def __iter__(self):
-        return iter(self.rwd_features)
-
-    def get_values(self, state):
-        """
-        Gets an array with the values / counts for each reward feature according to the given state. If the state is
-        probabilistic (distribution), then average among all possible states is retrieved, weight by the corresponding
-        probabilities.
-        :param VectorDistributionSet state: the world state from which to get the feature value.
-        :rtype: np.ndarray
-        :return: an array containing the values for each feature.
-        """
-        return np.array([feat.get_value(state) for feat in self.rwd_features])
-
-    def set_rewards(self, agent, weights, model=None):
-        """
-        Sets rewards to the agent corresponding to the value of each feature multiplied by the corresponding weight.
-        :param Agent agent: the agent to whom the reward is going to be set.
-        :param np.ndarray weights: the weights associated with each reward feature.
-        :param str model: the name of the agent's model for whom to set the reward.
-        :return:
-        """
-        assert len(weights) == len(self.rwd_features), \
-            'Provided weight vector\'s dimension does not match reward features'
-
-        agent.setAttribute('R', {})
-        for i, weight in enumerate(weights):
-            self.rwd_features[i].set_reward(agent, weight, model)
-
-
 class LinearRewardFeature(ABC):
     """
     Represents a reward feature that can be used in linearly-parameterized reward functions.
     """
 
-    def __init__(self, name, normalize_factor=1):
+    def __init__(self, name: str, normalize_factor: float = 1):
         """
         Creates a new reward feature.
         :param str name: the label for this reward feature.
@@ -73,24 +29,23 @@ class LinearRewardFeature(ABC):
         self.normalize_factor = normalize_factor
 
     @abstractmethod
-    def get_value(self, state):
+    def get_value(self, state: State) -> float:
         """
         Gets the value / count of the feature according to the given state. If the state is probabilistic
         (distribution), then average among all possible states is retrieved, weight by the corresponding probabilities.
-        :param VectorDistributionSet state: the world state from which to get the feature value.
+        :param State state: the world state from which to get the feature value.
         :rtype: float
-        :return: the (mean) value of the feature in the given state.
+        :return: the value of the feature in the given state (expected if state is distribution).
         """
         pass
 
     @abstractmethod
-    def set_reward(self, agent, weight, model=None):
+    def set_reward(self, agent: Agent, weight: float, model: Optional[str] = None):
         """
         Sets a reward to the agent corresponding to the value of the features multiplied by the given weight.
         :param Agent agent: the agent to whom the reward is going to be set.
         :param float weight: the weight associated with the reward feature.
         :param str model: the name of the agent's model for whom to set the reward.
-        :return:
         """
         pass
 
@@ -101,7 +56,7 @@ class ValueComparisonLinearRewardFeature(LinearRewardFeature):
     the comparison is satisfied, otherwise returns `0`.
     """
 
-    def __init__(self, name, world, key, value, comparison):
+    def __init__(self, name: str, world: World, key: str, value: PsychSimType, comparison: str):
         """
         Creates a new reward feature.
         :param World world: the PsychSim world capable of retrieving the feature's value given a state.
@@ -115,17 +70,17 @@ class ValueComparisonLinearRewardFeature(LinearRewardFeature):
         self.key = key
         self.value = value
         assert comparison in KeyedPlane.COMPARISON_MAP, \
-            'Invalid comparison provided: {}; valid: {}'.format(comparison, KeyedPlane.COMPARISON_MAP)
+            f'Invalid comparison provided: {comparison}; valid: {KeyedPlane.COMPARISON_MAP}'
         self.comparison = KeyedPlane.COMPARISON_MAP.index(comparison)
         self.comp_func = COMPARISON_OPS[comparison]
 
-    def get_value(self, state):
+    def get_value(self, state: State) -> float:
         # collects feature value distribution and returns weighted sum
         dist = np.array([[float(self.comp_func(self.world.float2value(self.key, kv[self.key]), self.value)), p]
                          for kv, p in state.distributions[state.keyMap[self.key]].items()])
         return dist[:, 0].dot(dist[:, 1]) * self.normalize_factor
 
-    def set_reward(self, agent, weight, model=None):
+    def set_reward(self, agent: Agent, weight: float, model: Optional[str] = None):
         rwd_key = rewardKey(agent.name)
         tree = {'if': KeyedPlane(KeyedVector({self.key: 1}), self.value, self.comparison),
                 True: setToConstantMatrix(rwd_key, 1.),
@@ -138,7 +93,7 @@ class ActionLinearRewardFeature(ValueComparisonLinearRewardFeature):
     Represents a reward feature that returns `1` if the agent's action is equal to a given action, and `0` otherwise.
     """
 
-    def __init__(self, name, agent, action):
+    def __init__(self, name: str, agent: Agent, action: str):
         """
         Creates a new reward feature.
         :param str name: the label for this reward feature.
@@ -153,7 +108,7 @@ class NumericLinearRewardFeature(LinearRewardFeature):
     Represents a numeric reward feature, that returns a reward proportional to the feature's value.
     """
 
-    def __init__(self, name, key):
+    def __init__(self, name: str, key: str):
         """
         Creates a new reward feature.
         :param str name: the label for this reward feature.
@@ -162,11 +117,57 @@ class NumericLinearRewardFeature(LinearRewardFeature):
         super().__init__(name)
         self.key = key
 
-    def get_value(self, state):
+    def get_value(self, state: State) -> float:
         # simply return expectation of marginal
         return state.marginal(self.key).expectation() * self.normalize_factor
 
-    def set_reward(self, agent, weight, model=None):
+    def set_reward(self, agent: Agent, weight: float, model: Optional[str] = None):
         # simply multiply the feature's value by the given weight
         rwd_key = rewardKey(agent.name)
         agent.setReward(KeyedTree(setToFeatureMatrix(rwd_key, self.key)), weight * self.normalize_factor, model)
+
+
+class LinearRewardVector(object):
+    """
+    Represents a linear reward vector, i.e., a reward function formed by a linear combination of a set of reward
+    features.
+    """
+
+    def __init__(self, rwd_features: List[LinearRewardFeature]):
+        """
+        Creates a new reward vector with the given features.
+        :param list[LinearRewardFeature] rwd_features: the list of reward features.
+        """
+        self.rwd_features = rwd_features
+        self.names: List[str] = [feat.name for feat in self.rwd_features]
+
+    def __len__(self):
+        return len(self.rwd_features)
+
+    def __iter__(self):
+        return iter(self.rwd_features)
+
+    def get_values(self, state: State) -> np.ndarray:
+        """
+        Gets an array with the values / counts for each reward feature according to the given state. If the state is
+        probabilistic (distribution), then average among all possible states is retrieved, weight by the corresponding
+        probabilities.
+        :param State state: the world state from which to get the feature value.
+        :rtype: np.ndarray
+        :return: an array containing the values for each feature.
+        """
+        return np.array([feat.get_value(state) for feat in self.rwd_features])
+
+    def set_rewards(self, agent: Agent, weights: np.ndarray, model: Optional[str] = None):
+        """
+        Sets rewards to the agent corresponding to the value of each feature multiplied by the corresponding weight.
+        :param Agent agent: the agent to whom the reward is going to be set.
+        :param np.ndarray weights: the weights associated with each reward feature.
+        :param str model: the name of the agent's model for whom to set the reward.
+        """
+        assert len(weights) == len(self.rwd_features), \
+            'Provided weight vector\'s dimension does not match reward features'
+
+        agent.setAttribute('R', {}, model)  # make sure to clear agent's reward function
+        for i, weight in enumerate(weights):
+            self.rwd_features[i].set_reward(agent, weight, model)
--- model_learning/features/objectworld.py
+++ model_learning/features/objectworld.py
@@ -1,5 +1,10 @@
 import copy
 import numpy as np
+from typing import List, Optional
+from model_learning.environments.gridworld import Location
+from psychsim.agent import Agent
+from model_learning import State
+from model_learning.environments.objects_gridworld import ObjectsGridWorld
 from model_learning.features.linear import LinearRewardVector
 
 __author__ = 'Pedro Sequeira'
@@ -12,25 +17,33 @@ class ObjectsRewardVector(LinearRewardVector):
     features.
     """
 
-    def __init__(self, env, agent, feat_matrix, outer=True, inner=True):
+    def __init__(self,
+                 env: ObjectsGridWorld,
+                 agent: Agent,
+                 feat_matrix: np.ndarray,
+                 outer: bool = True,
+                 inner: bool = True):
         """
         Creates a new reward vector with the given features.
         :param ObjectsGridWorld env: the objects environment used iby this reward function.
         :param Agent agent: the agent for which to get the location features.
-        :param np.ndarray feat_matrix: the feature matrix containing the color features for each environment location.
+        :param np.ndarray feat_matrix: the feature matrix containing the color features for each environment location,
+        shaped (width, height, num_colors [, num_colors]).
         :param bool outer: whether to include features for the presence of objects' outer colors.
         :param bool inner: whether to include features for the presence of objects' inner colors.
         """
         super().__init__([])
-        assert env.num_colors * outer + env.num_colors * inner == feat_matrix.shape[1], \
+        assert inner or outer, 'At least one option "inner" or "outer" must be true'
+        assert ((feat_matrix.shape[-1] == env.num_colors) and
+                (not (inner and outer) or feat_matrix.shape[-2] == env.num_colors)), \
             'Invalid feature matrix dimensions for provided inner and outer color arguments.'
         self.env = env
         self.x, self.y = env.get_location_features(agent)
         self.feat_matrix = feat_matrix
         self.outer = outer
         self.inner = inner
-        self.names = ['Out Color {}'.format(i + 1) for i in range(env.num_colors if outer else 0)] + \
-                     ['In Color {}'.format(i + 1) for i in range(env.num_colors if inner else 0)]
+        self.names: List[str] = [f'Out Color {i}' for i in range(env.num_colors if outer else 0)] + \
+                                [f'In Color {i}' for i in range(env.num_colors if inner else 0)]
 
     def __len__(self):
         return len(self.names)
@@ -38,40 +51,46 @@ class ObjectsRewardVector(LinearRewardVector):
     def __iter__(self):
         return iter(self.names)
 
-    def get_values(self, state):
+    def get_values(self, state: State) -> np.ndarray:
         """
         Gets an array with the values / counts for each reward feature according to the given state. If the state is
         probabilistic (distribution), then average among all possible states is retrieved, weight by the corresponding
         probabilities.
-        :param VectorDistributionSet state: the world state from which to get the feature value.
+        :param State state: the world state from which to get the feature value.
         :rtype: np.ndarray
         :return: an array containing the values for each feature.
         """
         # get agent's XY features
         state = copy.deepcopy(state)
-        state.collapse({self.x, self.y}, False)
+        state.collapse({self.x, self.y}, False)  # collapses into single joint distribution
 
         # collects possible locations and associated probabilities
-        locs = {}
+        locs: List[Location] = []
+        probs: List[float] = []
         for row in state.distributions[state.keyMap[self.x]].domain():
             x_val = row[self.x]
             y_val = row[self.y]
-            locs[self.env.xy_to_idx(x_val, y_val)] = state.distributions[state.keyMap[self.x]][row]
+            locs.append(Location(x_val, y_val))
+            probs.append(state.distributions[state.keyMap[self.x]][row])
 
         # return weighted average of feature vectors
-        return np.multiply(self.feat_matrix[list(locs.keys())],
-                           np.array(list(locs.values())).reshape(len(locs), 1)).sum(axis=0)
+        return np.multiply(self.feat_matrix[locs],
+                           np.array(probs).reshape(len(locs), 1)).sum(axis=0)
 
-    def set_rewards(self, agent, weights, model=None):
+    def set_rewards(self,
+                    agent: Agent,
+                    weights_outer: Optional[np.ndarray] = None,
+                    weights_inner: Optional[np.ndarray] = None,
+                    model: Optional[str] = None):
         """
         Sets a reward to the agent that is a linear combination of the given weights associated with each object color.
         :param Agent agent: the agent to whom the reward is going to be set.
-        :param np.ndarray weights: the reward weights associated with each object's outer and inner color.
+        :param np.ndarray weights_outer: the reward weights associated with each object's outer color.
+        :param np.ndarray weights_inner: the reward weights associated with each object's inner color.
         :param str model: the name of the agent's model for whom to set the reward.
         :return:
         """
-        assert len(weights) == len(self.names), 'Provided weight vector\'s dimension does not match reward features'
-        agent.setAttribute('R', {})
+        agent.setAttribute('R', {}, model)  # make sure to clear agent's reward function
         self.env.set_linear_color_reward(agent,
-                                         weights[:self.env.num_colors] if self.outer else None,
-                                         weights[self.env.num_colors:] if self.inner else None)
+                                         weights_outer if self.outer else None,
+                                         weights_inner if self.inner else None)
