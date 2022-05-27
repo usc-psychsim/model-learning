f667c9cfc0b1c052bf8bfe836c095380c41fd2a7
Pedro Sequeira <pedrodbs@gmail.com>
2022-05-26 19:18:07 -0700

    added collect_traejctories bin script for object_world with an example of linear reward function

--- model_learning/examples/collect_trajectories_objects.py
+++ model_learning/examples/collect_trajectories_objects.py
@@ -1,31 +1,29 @@
 import logging
 import os
 import numpy as np
-from psychsim.reward import maximizeFeature
-from psychsim.world import World
+from model_learning.environments.objects_gridworld import ObjectsGridWorld
 from model_learning.planning import get_policy, get_action_values
-from model_learning.environments.gridworld import GridWorld
 from model_learning.util.io import create_clear_dir, change_log_handler
+from psychsim.world import World
 
 __author__ = 'Pedro Sequeira'
 __email__ = 'pedrodbs@gmail.com'
-__description__ = 'Collects some trajectories in the normal gridworld with a reward function that tries to maximize ' \
-                  'both x and y coordinates of the agent\'s cell location.' \
-                  'Plots the trajectories and the reward and value functions.'
 
 ENV_SIZE = 10
+NUM_OBJECTS = 25
+NUM_COLORS = 5
 
 AGENT_NAME = 'Agent'
 HORIZON = 3
-RATIONALITY = 1 / 0.1  # inverse temperature
+RATIONALITY = 1 / 0.05  # inverse temperature
 ACTION_SEL = 'distribution'  # stochastic over all actions
 PRUNE_THRESHOLD = 1e-2
 SELECT = True
 
-NUM_TRAJECTORIES = 5  # 20
-TRAJ_LENGTH = 10  # 15
+NUM_TRAJECTORIES = 5  # 10
+TRAJ_LENGTH = 10  # 20
 
-OUTPUT_DIR = 'output/examples/collect-trajectories'
+OUTPUT_DIR = 'output/tests/objects-world'
 SEED = 0
 PROCESSES = 1
 VERBOSE = True
@@ -44,16 +42,28 @@ if __name__ == '__main__':
     agent.setAttribute('rationality', RATIONALITY)
 
     # create grid-world and add world dynamics to agent
-    env = GridWorld(world, ENV_SIZE, ENV_SIZE)
+    env = ObjectsGridWorld(world, ENV_SIZE, ENV_SIZE, NUM_OBJECTS, NUM_COLORS, seed=SEED)
     env.add_agent_dynamics(agent)
-    env.plot(os.path.join(OUTPUT_DIR, 'env.png'))
+    env.plot(os.path.join(OUTPUT_DIR, f'env.{IMG_FORMAT}'))
 
-    # set reward function (maximize xy location, ie always move top/right)
-    x, y = env.get_location_features(agent)
-    agent.setReward(maximizeFeature(x, agent.name), 1.)
-    agent.setReward(maximizeFeature(y, agent.name), 1.)
+    # set reward function (love object 0, hate object 1, object 2 is ok, others are distractions)
+    weights = np.array([0.4, -0.4, 0.1, 0., 0.])
+    env.set_linear_color_reward(agent, weights)
+    logging.info(f'Set reward function to agent:\n{agent.getReward()[agent.get_true_model()]}')
 
     world.setOrder([{agent.name}])
+
+    logging.info('Computing color feature maps...')
+    feat_matrix = env.get_location_feature_matrix(outer=True, inner=False)
+    for c in range(env.num_colors):
+        c_f = feat_matrix[..., c].T  # need to transpose XY
+        env.plot_func(c_f, os.path.join(OUTPUT_DIR, f'color_{c}_feat.{IMG_FORMAT}'), f'Color {c} Features')
+
+    feat_matrix = env.get_distance_feature_matrix(outer=False, inner=True)
+    for c in range(env.num_colors):
+        c_f = feat_matrix[..., c].T  # need to transpose XY
+        env.plot_func(c_f, os.path.join(OUTPUT_DIR, f'color_{c}_dist_feat.{IMG_FORMAT}'),
+                      f'Color {c} Distance Features')
 
     # generate trajectories using agent's policy
     logging.info('Generating trajectories...')
@@ -78,5 +88,3 @@ if __name__ == '__main__':
     logging.info('Computing rewards...')
     r = np.array([agent.reward(state) for state in states])
     env.plot_func(r, os.path.join(OUTPUT_DIR, f'reward.{IMG_FORMAT}'), 'Rewards')
-
-    logging.info('\nDone!')
