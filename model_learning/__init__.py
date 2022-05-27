f4e0860aa73d4c30f100772bf49f02d056bc91bd
Pedro Sequeira <pedrodbs@gmail.com>
2022-05-26 20:50:52 -0700

    moved custom types to module's root

--- model_learning/__init__.py
+++ model_learning/__init__.py
@@ -1,2 +1,12 @@
+from psychsim.probability import Distribution
+from psychsim.pwl import VectorDistributionSet
+from psychsim.world import World
+from typing import Union, List, Tuple
+
 __author__ = 'Pedro Sequeira'
 __email__ = 'pedrodbs@gmail.com'
+
+# types
+PsychSimType = Union[float, int, str]
+State = VectorDistributionSet
+Trajectory = List[Tuple[World, Distribution]]  # list of world-action (distribution) pairs
