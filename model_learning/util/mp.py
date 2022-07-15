2879db44df91b23dbba87399f2c06ecc16790024
Pedro Sequeira <pedrodbs@gmail.com>
2022-07-14 21:33:19 -0700

    modified mp utils to allow: (1) nested calls to run_parallel; (2) allow logging in subprocesses spawned via run_parallel to a main logger in another process

--- model_learning/util/mp.py
+++ model_learning/util/mp.py
@@ -1,6 +1,9 @@
+import multiprocessing as mp
+import os
 import tqdm
 from typing import Callable, Optional, List
-from joblib import Parallel, delayed
+from joblib import Parallel, delayed, parallel_backend
+from .logging import MultiProcessLogger, create_mp_log_handler
 
 __author__ = 'Pedro Sequeira'
 __email__ = 'pedro.sequeira@sri.com'
@@ -27,7 +30,11 @@ class _ProgressParallel(Parallel):
         self._pbar.refresh()
 
 
-def run_parallel(func: Callable, args: List, processes: Optional[int] = None, use_tqdm: bool = True) -> List:
+def run_parallel(func: Callable,
+                 args: List,
+                 processes: Optional[int] = None,
+                 use_tqdm: bool = True,
+                 mp_logging: bool = False) -> List:
     """
     Run the given function for each of the given arguments in parallel and returns  alist with the results.
     :param func: the function to be executed.
@@ -36,12 +43,38 @@ def run_parallel(func: Callable, args: List, processes: Optional[int] = None, us
     :param int processes: the number of parallel processes to use.  `-1` means all CPUs are used, `1` means no parallel
     computing, `<-1` means `(n_cpus + 1 + n_jobs)` are used, `None` is equivalent to `n_jobs=1`.
     :param bool use_tqdm: whether to show a progress bar during parallel execution.
+    :param  bool mp_logging: whether to use multiprocess logging.
     :rtype: list
     :return: a list with the results of executing the given function over each of the arguments. Indices will be
     aligned with the input arguments.
     """
+
+    # check processes
+    if processes == -1:
+        processes = os.cpu_count()
+    processes = min(processes, len(args), os.cpu_count())
+
+    # check multiprocess logging
+    if mp_logging:
+        # redirects function to _log_processor to assign log handler
+        assert MultiProcessLogger.queue is not None, 'MultiProcessLogger has not been created'
+        args = [(func, _a, MultiProcessLogger.queue) for _a in args]
+        func = _log_processor
+
     star = isinstance(args[0], tuple)  # star if function is multi-argument
-    if len(args) == 1:
-        processes = 1  # no need for parallelization
-    return _ProgressParallel(n_jobs=processes, use_tqdm=use_tqdm, total=len(args))(
-        delayed(func)(*(arg if star else [arg])) for arg in args)
+
+    with parallel_backend('loky', inner_max_num_threads=os.cpu_count() // processes):  # spread cpus per job
+        return _ProgressParallel(n_jobs=processes, use_tqdm=use_tqdm, total=len(args))(
+            delayed(func)(*(arg if star else [arg])) for arg in args)
+
+
+def _log_processor(func: Callable, args, queue: mp.Queue):
+    # set root logger of this process to redirect to queue
+    create_mp_log_handler(queue)
+
+    # override singleton queue in this process to allow for nested calls to `run_parallel`
+    MultiProcessLogger.queue = queue
+
+    # execute function
+    star = isinstance(args, tuple)  # star if function is multi-argument
+    func(*(args if star else [args]))
