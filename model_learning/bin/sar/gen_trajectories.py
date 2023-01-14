503e122ab2c31777dde4490ca0bafcf1f80ff2d8
Pedro Sequeira <pedrodbs@gmail.com>
2023-01-13 17:30:33 -0800

    correction to gen_trajectories

--- model_learning/bin/sar/gen_trajectories.py
+++ model_learning/bin/sar/gen_trajectories.py
@@ -6,7 +6,9 @@ from typing import get_args
 from model_learning import SelectionType
 from model_learning.bin.sar import create_sar_world, add_common_arguments, create_mental_models, DISCOUNT, HORIZON, \
     ACT_SELECTION, RATIONALITY
+from model_learning.util.cmd_line import save_args
 from model_learning.util.io import create_clear_dir, save_object
+from model_learning.util.logging import change_log_handler
 
 __author__ = 'Haochen Wu, Pedro Sequeira'
 __email__ = 'hcaawu@gmail.com, pedrodbs@gmail.com'
@@ -14,12 +16,18 @@ __maintainer__ = 'Pedro Sequeira'
 __description__ = 'Collects a set of fixed-length trajectories in the search-and-rescue domain. ' \
                   'Saves the trajectories to a gif animation and pickle file.'
 
-PRUNE_THRESHOLD = 1e-2
 NUM_TRAJECTORIES = 1  # 5  # 10
 TRAJ_LENGTH = 25  # 30
+TRAJECTORIES_FILE = 'trajectories.pkl.gz'
 
 
 def main():
+    # create output
+    output_dir = args.output
+    create_clear_dir(output_dir, clear=args.clear)
+    change_log_handler(os.path.join(output_dir, 'collect.log'), level=args.verbosity)
+    save_args(args, os.path.join(output_dir, 'args.json'))
+
     # create world and agents
     env, team, team_config, profiles = create_sar_world(args)
 
@@ -54,7 +62,7 @@ def main():
     env.play_team_trajectories(team_trajectories, anim_path)
 
     # save all trajectories to pickle file
-    file_path = os.path.join(output_dir, 'trajectories.pkl.gz')
+    file_path = os.path.join(output_dir, TRAJECTORIES_FILE)
     logging.info(f'Saving trajectories to {file_path}...')
     save_object(team_trajectories, file_path, compress_gzip=True)
 
@@ -76,8 +84,6 @@ if __name__ == '__main__':
     parser.add_argument('--trajectories', '-t', type=int, default=NUM_TRAJECTORIES,
                         help='Number of trajectories to generate.')
     parser.add_argument('--length', '-l', type=int, default=TRAJ_LENGTH, help='Length of the trajectories to generate.')
-    parser.add_argument('--prune', '-pt', type=float, default=PRUNE_THRESHOLD,
-                        help='Likelihood threshold for pruning outcomes during planning. `None` means no pruning.')
 
     args = parser.parse_args()
 
